b01b1c6b082c9bb70e11dfceb26cba1d9cf8851e
Yoeven D Khemlani <yoevenkhem@gmail.com>
2018-09-04 18:45:32 +0800

    First Commit

---
+++ SaveDataManager/Assets/SavaDataManager/Core/SaveDataManager.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using UnitySaveDataManager;
+
+public class SaveDataManager : MonoBehaviour
+{
+    public string SaveDataFileName = "Game";
+    
+    private static string _SaveDataFileName = "Game";
+    private static DataValues dataValues;
+
+    private void Awake()
+    {
+        _SaveDataFileName = SaveDataFileName;
+        if (!File.Exists(Application.persistentDataPath + "/" + _SaveDataFileName + ".dat"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Create(Application.persistentDataPath + "/" + _SaveDataFileName + ".dat");
+            dataValues = new DataValues();
+            bf.Serialize(file, dataValues);
+            file.Close();
+        }
+        load();
+    }
+
+    public static void SaveData(string key, object data)
+    {
+        dataValues.AddValue(key, data);
+        save();
+    }
+
+    public static bool Contains(string Key)
+    {
+        return dataValues.ContainsKey(Key);
+    }
+
+    public static T GetData<T>(string key)
+    {
+        return (T)dataValues.GetValue(key);
+    }
+
+    static void save()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/" + _SaveDataFileName + ".dat");
+        bf.Serialize(file, dataValues);
+        file.Close();
+    }
+
+    static void load()
+    {
+        if (File.Exists(Application.persistentDataPath + "/" + _SaveDataFileName + ".dat"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/" + _SaveDataFileName + ".dat", FileMode.Open);
+            dataValues = (DataValues)bf.Deserialize(file);
+            file.Close();
+        }
+    }
+
+}
+
+namespace UnitySaveDataManager
+{
+    [Serializable]
+    class DataValues
+    {
+        Dictionary<string, object> Datas;
+
+        public DataValues()
+        {
+            Datas = new Dictionary<string, object>();
+        }
+
+        public void AddValue(string key, object value)
+        {
+            if (Datas.ContainsKey(key))
+            {
+                Datas[key] = value;
+            }
+            else
+            {
+                Datas.Add(key, value);
+            }
+        }
+
+        public bool ContainsKey(string key)
+        {
+            return Datas.ContainsKey(key);
+        }
+
+        public object GetValue(string key)
+        {
+            if (Datas.ContainsKey(key))
+            {
+                return Datas[key];
+            }
+            Debug.LogError("Key doesn't exist");
+            return null;
+        }
+    }
+}
+
---
+++ SaveDataManager/Assets/SavaDataManager/Examples/SavaDataManagerExample.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SavaDataManagerExample : MonoBehaviour
+{
+    public Text KeyInputText;
+    public Text ValueInputText;
+    public Text KeyInputFindDataText;
+    public Text DisplayValueText;
+
+    public void SaveData()
+    {
+        SaveDataManager.SaveData(KeyInputText.text, ValueInputText.text);
+    }
+
+    public void GetData()
+    {
+        if (SaveDataManager.Contains(KeyInputFindDataText.text))
+        {
+            DisplayValueText.text = SaveDataManager.GetData<string>(KeyInputFindDataText.text);
+        }
+        else
+        {
+            DisplayValueText.text = "Data doesn't exist";
+        }
+    }
+}
